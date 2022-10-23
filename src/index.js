731b7279829abc93e6467e256dae33e935f81553
Masahiro Takata <takata005581016@gmail.com>
2022-10-23 21:56:52 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>こんにちは</h1>
+      <p>元気ですか？？</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
